78450c75c8e3c82736c84483e1ed89a882ef5670
kclaka <kennyigbechi@gmail.com>
2020-03-15 01:13:51 -0700

    Add project files.

---
+++ nameSplit/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace nameSplit 
+{ 
+
+   
+
+    class Program
+    {
+
+    static void splitName(string name, out string firstNames, out string lastName)
+    {
+        int i = name.LastIndexOf(' ');
+        firstNames = name.Substring(0, i);
+        lastName = name.Substring(i + 1);
+    }
+    static void Main(string[] args)
+        {
+        
+        splitName("Van Hugh Das", out string first, out string last);
+            Console.WriteLine(first);
+            Console.WriteLine(last);
+        }
+    }
+}
